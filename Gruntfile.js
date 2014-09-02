b45b3e042d3f1019a32c75dfb2b18bd66b9eaa1f
Ben Liu <benliu723@gmail.com>
2014-09-02 15:48:51 +0800

    first commit for structure

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+	var config = {
+		pkg: grunt.file.readJSON('package.json'),
+		formlyConfig: {
+			hostname: 'localhost', // change to 0.0.0.0 to listen on all connections
+			demo: 'demo',
+			port: 4000,
+			livereloadport: 35701
+		},
+		connect: {
+			dev: {
+				options: {
+					hostname: '<%= formlyConfig.hostname %>',
+					port: '<%= formlyConfig.port %>',
+					base: '<%= formlyConfig.demo %>',
+					livereload: '<%= formlyConfig.livereloadport %>'
+				}
+			}
+		},
+		watch: {
+			livereload: {
+				files: ['<%= formlyConfig.demo %>/**/*.{js,css,html}'],
+				options: {
+					livereload: '<%= formlyConfig.livereloadport %>'
+				}
+			}
+		},
+
+	};
+
+	// Pass config to grunt
+	grunt.initConfig(config);
+
+	grunt.registerTask('dev', [
+		'connect:dev',
+		'watch'
+	]);
+
+	grunt.registerTask('default', ['dev']);
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+FromlyAngularStrap
+
+This is an demo by using AngularFormly with AngularStrap, which uses andvantages from both of them.
+
+To run it with the following staps:
+
+		npm install
+
+		bower install
+
+		grunt dev
+
+And then you can visit your page with url: 
+
+		locahost:4000 
+
+	or
+
+		localhost:4000/index.html
